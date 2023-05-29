8d0bcad83511d971bdcb0daa895cbf37e3be5cff
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-05-29 19:44:07 +0200

    timer.app 16.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import Button from "./components/Button/Button";
+import Container from "./components/Container/Container";
+import Timer from "./components/Timer/Timer";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
 const App = () => {
+
+
+  const [ time, setTime ] = useState(0);
+  const [ timerOn, setTimeOn] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (timerOn) {
+        interval = setInterval(()  => {
+            setTime(prevTime => prevTime + 10)
+        }, 10)
+    } else {
+        clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+}, [timerOn])
+
   return (
-    <h1>Hello world</h1>
+    <Container>
+    <Timer time={time}></Timer>
+    <Button onClick={() => setTimeOn(true)}>START</Button>
+    <Button onClick={() => setTimeOn(false)}>STOP</Button>
+    <Button onClick={() => setTime(0)}>RESET</Button>
+    </Container>
   );
+
 };
 
 export default App;
